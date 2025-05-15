41cf5a0eab5deaed58a10318ed0bb06efa275e7b
laysoon <163278347+laysoonchris@users.noreply.github.com>
2025-05-16 00:04:35 +0200

    When db is added its finished

---
+++ IDbService.cs
@@ -0,0 +1,10 @@
+﻿using Tutorial9.Model;
+using Tutorial9.Model.DTOs;
+
+namespace Tutorial9.Services;
+
+public interface IDbService
+{
+    Task<int> AddProductToWarehouseAsync(ProductWarehouseDTO productWarehouse);
+    Task<int> ProcedureAsync(ProductWarehouseDTO productWarehouse);
+}
