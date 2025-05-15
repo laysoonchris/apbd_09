41a32f13e3ed3fc084e83e0d99a5391990c07b00
laysoon <163278347+laysoonchris@users.noreply.github.com>
2025-05-16 00:00:36 +0200

    Almost finished

---
+++ Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial9.Model;
+
+public class Product
+{
+    public int IdProduct { get; set; }
+    [MaxLength(200)]
+    public string Name { get; set; }
+    [MaxLength(200)]
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+}
